1789652342deaae3b3adc9ce192d2df1d3c00707
Moxy <keanu.knighton@gmail.com>
2023-03-03 19:56:40 +0000

    index.js profile generation functions beginning#

--- code/index.js
+++ code/index.js
@@ -13,3 +13,42 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const idList = []
+const teamMembers = []
+
+
+const appMenu = () => {
+    function createManager(){
+        console.log("Begin buidling your team.");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's name?",
+            },
+
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the team manager's Id?",
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email?"
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the team manager's office number?"
+            },
+
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers,managerId, answers.managerEmail, answers.managerOfficeNumber)
+            teamMembers.push(manager);
+            idList.push(answers.managerId);
+        })
+
+
+    }
+}
--- code/lib/Employee.js
+++ code/lib/Employee.js
@@ -1,15 +1,6 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email) {
-        if (!name) {
-        throw new Error("You are missing: Name.");
-        }
-        if (!id) {
-          throw new Error("You are missing: ID.");
-        }
-        if (!email) {
-          throw new Error("You are missing: Email.");
-        }
         this.name = name;
         this.id = id;
         this.email = email;
@@ -32,4 +23,6 @@ getRole() {
   return "Employee";
 }
 
-};
+}
+
+module.exports = Employee;
