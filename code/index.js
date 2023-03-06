69a43dcad3407386100a4dc106855005ca9f7ecb
Moxy <keanu.knighton@gmail.com>
2023-03-06 15:50:37 +0000

    completeTeam function

--- code/index.js
+++ code/index.js
@@ -19,9 +19,11 @@ const teamMembers = []
 
 const appMenu = () => {
 
-    
     function completeTeam() {
-        
+        if(!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), 'utf-8');
     }
 
     function addEngineer() {
